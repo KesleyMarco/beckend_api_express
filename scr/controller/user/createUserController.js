14261644bbca6041d4f3313a5531f53d8e80ce68
KesleyMarco <kesleyreis07@gmail.com>
2026-03-17 19:24:54 -0300

    feat: update user creation logic and add database connection helper

--- scr/controller/user/createUserController.js
+++ scr/controller/user/createUserController.js
@@ -5,12 +5,15 @@ export async function createUserController(req, res) {
   const user = {
     avatar: "https://github.com/kesleymarco.png",
     name: "kesleymarco",
-    email: "kesleyreis07@gmail.com"
-    pass: "123456"
+    email: "kesleyreis07@gmail.com",
+    pass: "12345678"
 
   }
 
   const result = await createUser(user)
 
-  res.json
+  res.json({
+    message: "User created successfully",
+    user: result
+  })
 }
